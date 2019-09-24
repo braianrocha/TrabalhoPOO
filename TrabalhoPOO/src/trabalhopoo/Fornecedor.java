55ea2cf29efb98fdb2cc4163db357e455822ab87
braian <braianluz@gmail.com>
2019-09-23 21:31:55 -0300

    Ruan att
    Atualização RUAN

--- TrabalhoPOO/src/trabalhopoo/Fornecedor.java
+++ TrabalhoPOO/src/trabalhopoo/Fornecedor.java
@@ -8,19 +8,15 @@ public class Fornecedor {
     private int id;
     private String nome;
     private String cpfCnpj;
-    private String tipoProduto;
     private String contato;
-    private String produto;
     private String endereco;
     private boolean ativo;
 
-    public Fornecedor(int id,String nome, String cpfCnpj, String tipoProduto, String contato, String produto, String endereco, boolean ativo) {
+    public Fornecedor(int id,String nome, String cpfCnpj, String contato, String endereco, boolean ativo) {
         this.id = id;
         this.nome = nome;
         this.cpfCnpj = cpfCnpj;
-        this.tipoProduto = tipoProduto;
         this.contato = contato;
-        this.produto = produto;
         this.endereco = endereco;
         this.ativo = ativo;
     }
@@ -31,31 +27,28 @@ public class Fornecedor {
 
         Scanner solicitaDado = new Scanner(System.in);
 
-        System.out.println("Informe o nome para Identificar seu Fornecedor: ");
+        System.out.println("Informe o codigo para Identificar seu Fornecedor: ");
         int id = solicitaDado.nextInt();
         
+         solicitaDado = new Scanner(System.in);
+                
         System.out.println("Informe o Nome do Fornecedor: ");
-        String nome = solicitaDado.next();
+        String nome = solicitaDado.nextLine();
 
         System.out.println("Informe o CPF ou o CNPJ: ");
-        String cpfCnpj = solicitaDado.next();
+        String cpfCnpj = solicitaDado.nextLine();
 
         System.out.println("Informe o Endereco: ");
-        String endereco = solicitaDado.next();
+        String endereco = solicitaDado.nextLine();
 
         System.out.println("Informe os dados para contato: ");
-        String contato = solicitaDado.next();
+        String contato = solicitaDado.nextLine();
 
-        System.out.println("Informe o nome do Produto: ");
-        String produto = solicitaDado.next();
-
-        System.out.println("Informe o tipo do Produto: ");
-        String tipoProduto = solicitaDado.next();
 
         System.out.println("O Forncedor encontra-se ativo? ");
         boolean ativo = solicitaDado.nextBoolean();
 
-        getFornecedor().add(new Fornecedor(id,nome, cpfCnpj, tipoProduto, contato, produto, endereco, ativo));
+        getFornecedor().add(new Fornecedor(id,nome, cpfCnpj, contato, endereco, ativo));
         qtdFornecedor++;
 
     }
@@ -65,21 +58,27 @@ public class Fornecedor {
             System.out.println("Identificador do Fornecedor: " + fornecedor.get(i).getId()
                     +"\nNome: " + fornecedor.get(i).getNome()
                     + "\nCPF / CNPJ: " + fornecedor.get(i).getCpfCnpj()
-                    + "\nTipo do Produto: " + fornecedor.get(i).getTipoProduto()
                     + "\nContato: " + fornecedor.get(i).getContato()
                     + "\nEndereco: " + fornecedor.get(i).getEndereco());
         }
     }
-    //public buscaFornecedor buscar(String cpfCnpj)
     
-    
-    
-    
-    
-    
-    
-    
-    
+    //Busca Recebe ID do fornecedor
+    public static Fornecedor buscaFornecedor(int id) {
+        for (int i = 0; i < fornecedor.size(); i++) {
+            if (fornecedor.get(i).getId() == (id)) {
+                if (!(fornecedor.get(i).isAtivo())) {
+                    return null;
+                } else {               
+                    return fornecedor.get(i);
+
+                }
+            }
+
+        }
+        return null;
+
+    }
 
     public String getNome() {
         return nome;
@@ -97,28 +96,12 @@ public class Fornecedor {
         this.cpfCnpj = cpfCnpj;
     }
 
-    public String getTipoProduto() {
-        return tipoProduto;
-    }
-
-    public void setTipoProduto(String tipoProduto) {
-        this.tipoProduto = tipoProduto;
-    }
-
     public String getContato() {
         return contato;
     }
 
     public void setContato(String contato) {
         this.contato = contato;
-    }
-
-    public String getProduto() {
-        return produto;
-    }
-
-    public void setProduto(String produto) {
-        this.produto = produto;
     }
 
     public String getEndereco() {
